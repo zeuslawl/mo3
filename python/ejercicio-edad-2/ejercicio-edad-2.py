cee4fe8df9eb31e14163ae1f5f00c0704e7a0900
root <zeuslawl>
2017-02-27 13:58:55 +0100

    actualizado

---
+++ python/ejercicio-edad-2/ejercicio-edad-2.py
@@ -0,0 +1,11 @@
+# coding: utf­8
+
+"""
+Si tiene entre 18 y 23 años, entonces puedes pasar a la sesión de youngster
+"""
+edad = input("Introduce la edad:")
+
+if edad >=18 and edad <=23 : 
+	print "Hola"
+	print "Puedes pasar"
+
