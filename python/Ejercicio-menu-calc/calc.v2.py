52291281825ea4283b874fa047cb1dbb31d6633c
Alejandro <alejandrozeus21@gmail.com>
2017-03-15 13:31:37 +0100

    Create calc.v2.py

---
+++ python/Ejercicio-menu-calc/calc.v2.py
@@ -0,0 +1,39 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+"""
+Calculadora Básica
+""" 
+
+print 'Qué desea hacer el amo?'
+print '1.- Sumar'
+print '2.- Restar'
+print '3.- Multiplicar'
+print '4.- Dividir'
+print '5.- Salir'
+
+ 
+opcion = raw_input('Elija una opcion ---> ')
+if opcion == '1':
+	numero1 = float(raw_input('Introduce un numero -> '))
+	numero2 = float(raw_input('Introduce otro numero -> '))
+	
+	suma = numero1 + numero2
+	print 'El resultado es ', suma
+ 
+if opcion == '2':
+    resta = numero1 - numero2
+    print 'El resultado es ', resta
+ 
+if opcion == '3':
+    multi = numero1 * numero2
+    print 'El resultado es ', multi
+ 
+if opcion == '4':
+    divi = numero1 / numero2
+    print 'El resultado es ', divi
+    
+if opcion == '5':
+	exit
+	print 'Has salido'
