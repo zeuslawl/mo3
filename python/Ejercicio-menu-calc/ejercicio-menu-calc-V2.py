87664b86845dcf07cd67507e81f7bbcf4cd19995
Alejandro <alejandrozeus21@gmail.com>
2017-03-16 12:44:48 +0100

    Create ejercicio-menu-calc-V2.py

---
+++ python/Ejercicio-menu-calc/ejercicio-menu-calc-V2.py
@@ -0,0 +1,19 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+print 'Qué desea hacer el amo?'
+print 'S.- Salir'
+print '1.- Sumar'
+print '2.- Restar'
+print '3.- Multiplicar'
+print '4.- Dividir'
+
+opcion = raw_input('Elija una opcion ---> ')
+
+os.system('clear')
+
+if opcion == ("1" => and opcion == <= "4") or (opcion == "S") or (opcion= "s") :
+	print "Correcto"
+else:
+	print " Error, esa opcion no existe"
