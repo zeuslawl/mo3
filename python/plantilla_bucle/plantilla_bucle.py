e68cdc74a1e4a4ff492230c88b55ec2ff36d1874
Alejandro <alejandrozeus21@gmail.com>
2017-03-20 15:55:49 +0100

    Create plantilla_bucle.py

---
+++ python/plantilla_bucle/plantilla_bucle.py
@@ -0,0 +1,15 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+import os
+os.system('clear')
+
+salir = False
+
+while(salir==False) :
+
+		print("hola")
+		tecla = raw_input("Para salir teclee s o S ")
+		if(tecla=="S") or (tecla =="s") :
+			print ("Adiós")
+			salir=True
