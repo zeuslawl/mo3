6a1ceab11ee761f50556a2481de11f895315c1a8
Alejandro <alejandrozeus21@gmail.com>
2017-03-23 08:30:11 +0100

    Create ejercicio-bucle-contar2.py

---
+++ python/ejercicio-bucle-contar2.py/ejercicio-bucle-contar2.py
@@ -0,0 +1,21 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+
+import os
+os.system('clear')
+
+sortir = False
+numero = 1
+final = input("Hasta que número desea el amo ")
+
+while (sortir == False) :
+	
+	print numero
+	
+	if ( numero == final ) :
+		
+		sortir = True
+		
+	numero = numero +1
+	
