6538075f7513628d1fd219554812f94d8c50a448
Alejandro <alejandrozeus21@gmail.com>
2017-03-23 08:19:20 +0100

    Create ejercicio-bucle-contar.py

---
+++ python/ejercicio-bucle-contar.py/ejercicio-bucle-contar.py
@@ -0,0 +1,18 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+
+import os
+os.system('clear')
+
+numero = 1
+sortir = False
+
+while sortir == False :
+	print "El número es", numero
+	
+	if(numero == 50) :
+		sortir = True
+		 
+	numero = numero + 1
+	
