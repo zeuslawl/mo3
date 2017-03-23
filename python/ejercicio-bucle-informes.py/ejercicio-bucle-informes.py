2e697b62b436345aa69a92e743418ceb46242ca5
Alejandro <alejandrozeus21@gmail.com>
2017-03-23 08:38:39 +0100

    Create ejercicio-bucle-informes.py

---
+++ python/ejercicio-bucle-informes.py/ejercicio-bucle-informes.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+anyo = 2010
+sortir = False
+
+while sortir == False :
+	
+	print "Informe del año", anyo
+	if (informe == 2016) :
+		sortir = True
+		
+		anyo = anyo +1
