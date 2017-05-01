754cb71dfc7b4586272d74d66aaa4b66d3469c7d
Alejandro <alejandrozeus21@gmail.com>
2017-05-01 13:58:47 +0200

    Create contador_1-3.py

---
+++ python/contador_1-3.py/contador_1-3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from random import randint
+
+contador=1
+salir= False
+
+while(salir==False):
+	print contador
+	if(contador==3):
+		salir=True
+	contador= contador +1
