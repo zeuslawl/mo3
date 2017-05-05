2b32601bcc8e19254265a91f4581d1ae8288ebd0
Alejandro <alejandrozeus21@gmail.com>
2017-05-05 08:43:14 +0200

    Update for_ejemplo_examen.py

--- python/for_ejemplo_examen.py/for_ejemplo_examen.py
+++ python/for_ejemplo_examen.py/for_ejemplo_examen.py
@@ -3,23 +3,20 @@
 
 
 for fil in xrange(1,5,1):
+	print " "
 	for col in xrange(1,4,1):
 		if (fil == 1):
 			if (col ==2):
-				print "A"
+				print "A",
 			elif col == 3 :
-				print "B"
+				print "B",
 			elif col == 4 : 
-				print "C"
+				print "C",
 		else: 
-			print "-"
+			print "-",
 		if (col==1):
 			print fil-1
-		
-		if(fil==3 and col ==3):
-			print "*"
-		
-		if (fil==4 and col==2):
-			print "*"
+		else:
+			print "-",
 		
 	
