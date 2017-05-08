68a198d8237c17f1fd7f27af3c91acdcc9c06ca2
Alejandro <alejandrozeus21@gmail.com>
2017-05-08 13:21:23 +0200

    Update for_ejemplo_examen.py

--- python/for_ejemplo_examen.py/for_ejemplo_examen.py
+++ python/for_ejemplo_examen.py/for_ejemplo_examen.py
@@ -1,25 +1,31 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# !/usr/bin/python
+# -*-coding: utf-8-*-
 
+def my_range(inici,fi,increment):
+	while inici <= fi:
+		yield inici
+		inici = inici + increment
 
-for fil in xrange(1,6,1):
-	print " "
-	for col in xrange(1,5,1):
-		if (fil == 1):
+for fil in my_range(1,5,1):
+	for col in my_range(1,4,1):
+		if (fil ==1):
 			if (col ==2):
 				print "A",
-			elif col == 3 :
+			elif col ==3:
 				print "B",
-			elif col == 4 : 
+			elif col==4:
 				print "C",
-			else: 
-				print "-",
-		else: 
-			
-			if (col==1):
-				print fil-1,
 			else:
 				print "-",
+		else:
+			if(col==1):
+				print fil-1,
+			else:
+				if(fil==3 and col ==3) or (fil==4 and col==2):
+					print "*",
+				else:
+					print "-",
+	print ""
 
 		
 	
