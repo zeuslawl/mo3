6e4dc35c419db95708b615ffa03915b54046c879
Alejandro <alejandrozeus21@gmail.com>
2017-05-10 13:20:43 +0200

    Update examen_sin_cara.py

--- python/examen_sin_cara.py/examen_sin_cara.py
+++ python/examen_sin_cara.py/examen_sin_cara.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# !/usr/bin/python
+# -*-coding: utf-8-*-
 
 def my_range(inici,fi,increment):
 	while inici <= fi:
@@ -8,12 +8,13 @@ def my_range(inici,fi,increment):
 
 for fil in my_range(1,4,1):
 	for col in my_range(1,8,1):
-		if((fil==1) or (fil==4)):
+		if(fil==1):
+			print "*",
+		elif (fil==4):
 			print "*",
 		else:
-			if((col==1) or (col==8)):
+			if(fil==2 and col==1) or (fil==2 and col==8) or (fil==3 and col==1) or (fil==3 and col==8):
 				print "*",
 			else:
-				print "",
-		
-	print " "
+				print " ",
+	print ""
