6ee3272908e679d94e2e1d1c90e7733ff9191006
Alejandro <alejandrozeus21@gmail.com>
2017-05-21 12:37:55 +0200

    Con comentarios.

---
+++ python/ejercicio-bucle-informes.py/ejercicio-bucle-informes_comentarios.py
@@ -0,0 +1,16 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+#Imprime los informes. Desde el año 2010 hasta el 2016
+anyo = 2010
+sortir = False
+
+while sortir == False :
+	#Imprime los informes de los años.
+	print "Informe del año", anyo
+  #Cuando llegue a 2016, termina el bucle.
+	if ( anyo == 2016) :
+	
+		sortir = True
+
+anyo = anyo +1
