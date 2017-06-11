22278347fe7e5a0eaa59ff2b056fde9d820cf2d1
Alejandro <alejandrozeus21@gmail.com>
2017-06-11 13:39:56 +0200

    Create permisos.py

---
+++ python/permisos_Seguridad.py/permisos.py
@@ -0,0 +1,23 @@
+# !/usr/bin/python
+# coding:utf-8
+import os
+import stat
+ 
+path_to_explore="/home/zeus/Documentos/python"
+total_size=0
+   	 
+# Mostrem ruta tot    
+for root, dirs, files in os.walk(path_to_explore):
+    for name in files:
+    	name_path=os.path.join(root, name)
+    	print(name_path) ,
+    	print os.stat(name_path).st_size
+    	total_size=total_size+os.stat(name_path).st_size
+    	permissions = stat.S_IMODE ( os.stat (name_path).st_mode )
+    	print oct(permissions)
+ 
+ 
+    for name in dirs:
+    	name_path=os.path.join(root, name)
+    	print(name_path) ,
+    	print os.stat(name_path).st_size
